38330e20811766b5f653cc8d36a199714c622df3
bgerman@nearsoft.com <bgerman@nearsoft.com@0c66d552-755f-5c7f-8f86-71f2f8fd6789>
2012-05-24 02:11:53 +0000

    added gamecanvas and added graphicsengine capabilities

--- trunk/src/main/java/com/java2dgame/app/Drawable.java
+++ trunk/src/main/java/com/java2dgame/app/Drawable.java
@@ -1,8 +1,12 @@
 package com.java2dgame.app;
 
+import java.awt.Image;
+
 public interface Drawable {
 
 	public int getAssignedIndex();
 	public int setAssignedIndex(int index);
-
+	public Image getImage();
+	public int getScreenlocationY();
+	public int getScreenlocationX();
 }
